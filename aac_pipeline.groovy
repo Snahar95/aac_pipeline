5de81c1dfc20783383dd42da8413ab7d652c4efd
Snahar95 <shishirkona2002@yahoo.com>
2021-01-30 18:42:03 -0500

    my aac_pipeline commit 0130

--- aac_pipeline.groovy
+++ aac_pipeline.groovy
@@ -31,8 +31,8 @@ pipeline {
         }
     stage('Ansible Deploy') {
         steps{
-             withCredentials([sshUserPrivateKey(credentialsId: '2be93d5b-70f9-4db2-adc3-f307b7b86c48', keyFileVariable: 'myKEY')]) {
-                sh 'ansible-playbook ./ansible/playbooks/deploy_trainingApp.yml --user ubuntu --key-file ${myKEY}'  
+             withCredentials([sshUserPrivateKey(credentialsId: '52fb1edb-d474-4431-a176-6f5baba63704', keyFileVariable: 'myKey')]) {
+               // sh 'ansible-playbook ./ansible/playbooks/deploy_trainingApp.yml --user ubuntu --key-file ${myKey}'  
             }//end withCredentials
       }//end steps
     }//end stage
